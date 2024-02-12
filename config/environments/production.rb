01ac90daddc04bbc62c366396b5238737b886896
Mark Miranda <weatherby@m2-MBA>
2024-02-12 04:39:06 -0700

    update action_mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = {host: "markmiranda.org", port: 3000}
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { :api_token => ENV["POSTMARK_TOKEN"] }
+  config.action_mailer.default_url_options = {host: "steadfast-indecency.onrender.com", port: 3000}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.postmarkapp.com",
+    port: 587,
+    domain: "steadfast-indecency.onrender.com",
+    user_name: ENV["POSTMARK_TOKEN"],
+    password: ENV["POSTMARK_TOKEN"],
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
