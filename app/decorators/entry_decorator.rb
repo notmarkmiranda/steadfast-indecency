3f8abe65bad93880d843137b80bc07d1b523eb8d
Mark Miranda <weatherby@m2-MBA>
2024-03-10 01:21:52 -0700

    fix members table

--- app/decorators/entry_decorator.rb
+++ app/decorators/entry_decorator.rb
@@ -6,25 +6,31 @@ class EntryDecorator < Draper::Decorator
   end
 
   def delete_button
-    h.button_to h.pool_entry_path(object.pool, object), method: :delete, class: "px-3 py-2 text-white bg-red-500 border-red-500 rounded-lg hover:bg-red-600", form: {data: {turbo_confirm: "Are you sure? \n 🚨 This cannot be undone. 🚨"}} do
+    h.button_to h.pool_entry_path(object.pool, object), method: :delete, class: "px-3 py-1 text-white bg-red-500 border-red-500 rounded-lg hover:bg-red-600", form: {data: {turbo_confirm: "Are you sure? \n 🚨 This cannot be undone. 🚨"}} do
       "Delete Entry"
     end
   end
 
   def paid_unpaid_button
     if object.paid
-      h.button_to h.unpaid_pool_entry_path(object.pool, object), class: "px-3 py-2 text-white bg-red-500 border-red-500 rounded-lg hover:bg-red-600" do
+      h.button_to h.unpaid_pool_entry_path(object.pool, object), class: "px-2 py-1 text-white bg-red-500 border-red-500 rounded-lg hover:bg-red-600" do
         "Mark as Unpaid"
       end
     else
-      h.button_to h.paid_pool_entry_path(object.pool, object), class: "px-3 py-2 text-white bg-green-500 border-green-500 rounded-lg hover:bg-green-600" do
+      h.button_to h.paid_pool_entry_path(object.pool, object), class: "px-2 py-1 text-white bg-green-500 border-green-500 rounded-lg hover:bg-green-600" do
         "Mark as Paid"
       end
     end
   end
 
   def paid_icon
-    object.paid ? "✅ " : "❌ "
+    if object.paid
+      h.content_tag :i, nil, class: "fa-solid fa-star text-green-500"
+    else
+      h.content_tag :span, class: "text-yellow-500" do
+        h.content_tag :i, nil, class: "fa-solid fa-circle-xmark"
+      end
+    end
   end
 
   # Define presentation-specific methods here. Helpers are accessed through
